ea006f006a9a8afb852aaa6473503c99b6aa67bf
KamilaBeketova <you@example.com>
2023-07-27 02:07:41 +0200

    создала java script

---
+++ hw-2/index.js
@@ -0,0 +1,60 @@
+// Задание 1
+let a = 10;
+alert(a);
+let a = 20;
+alert(a);
+// Задание 2
+let age = 'Презентация первого iPhone была проведена 9 января 2007 года'
+alert(age)
+// Задание 3
+let userName = 'Брендан Эйх'
+alert(userName)
+// Задание 4
+let a = 10;
+let b = 2;
+alert(a + b)
+// Задание 5
+let a = 9;
+let result = a ** 5	let b = 2;
+let result = a % b
+alert(result)
+// Задание 6
+let a = 9;
+let result = a ** 5	let b = 2;
+let result = a % b
+alert(result)
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+// Задание 7
+let age = prompt('Сколько вам лет?')
+alert(age)
+// Задание 8
+let age = prompt('Сколько вам лет?')
+alert(age)
+// Задание 9.0
+const user = {
+    name: "Григорий",
+    age: "28",
+    isAdmin: "false"
+}
+// Задание 9.1
+console.log(user);
+user['city of residence'] = "Москва"
+// Задание 9.2
+user.age = "30"
+// Задание 9.3
+delete user['city of residence']
+// Задание 9.4
+let user = 'name'
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info])
+// Задание 10
+let name = Григорий;
+let info = prompt(Как вас зовут?)
